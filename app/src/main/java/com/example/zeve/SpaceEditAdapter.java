2107ba61eb788cfbd7cb83c2371cab131c891827
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-11-17 16:45:48 +0900

    Update SpaceEditAdapter.java

--- app/src/main/java/com/example/zeve/SpaceEditAdapter.java
+++ app/src/main/java/com/example/zeve/SpaceEditAdapter.java
@@ -1,11 +1,17 @@
 package com.example.zeve;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -22,6 +28,28 @@ public class SpaceEditAdapter extends RecyclerView.Adapter<SpaceEditAdapter.Item
     @Override
     public void onBindViewHolder(@NonNull ItemViewHolder holder, int position){
         holder.onBind(data.get(position));
+        Context context=holder.itemView.getContext();
+        ImageView imageByCode=new ImageView(context);
+        imageByCode.setImageResource(data.get(position).getMyItemImg());
+        ConstraintLayout.LayoutParams layoutLayoutParams = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.MATCH_PARENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT
+        );
+        imageByCode.setLayoutParams(layoutLayoutParams);
+        holder.imageView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+//                ConstraintLayout.LayoutParams imgParams = new ConstraintLayout.LayoutParams(
+//                        ConstraintLayout.LayoutParams.WRAP_CONTENT,
+//                        ConstraintLayout.LayoutParams.WRAP_CONTENT
+//                );
+               // holder.imageView.setLayoutParams(imgParams);
+                holder.spaceLay.addView(imageByCode);
+
+
+            }
+        });
     }
     @Override
     public int getItemCount(){
@@ -33,15 +61,16 @@ public class SpaceEditAdapter extends RecyclerView.Adapter<SpaceEditAdapter.Item
 
     class ItemViewHolder extends RecyclerView.ViewHolder{
         private ImageView imageView;
-
+        private ConstraintLayout spaceLay;
         ItemViewHolder(View itemView){
             super(itemView);
             imageView=itemView.findViewById(R.id.myItemImg);
-
+            spaceLay=itemView.findViewById(R.id.spaceLay);
         }
         void onBind(SpaceEditData data){
             imageView.setImageResource(data.getMyItemImg());
 
         }
     }
+
 }
